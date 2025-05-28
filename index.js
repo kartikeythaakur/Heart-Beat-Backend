6e41eb690e9ac97183e3f38f8eac46a554d3f25d
Kartikey Thakur <kartikeyrajput90@gmail.com>
2025-05-28 12:20:05 +0530

    4th commit

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { ConnectToDB } = require("./config/connection");
+const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
-const { ConnectToDB } = require("./config/connection");
+
+const userRoute = require("./Routes/userRoute");
+const otpRoute = require("./Routes/otpRoute");
+const likeRoute = require("./Routes/likeRoute");
+const chatRoute = require("./Routes/chatRoute");
+
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
   },
 });
-const userRoute = require("./Routes/userRoute");
-const otpRoute = require("./Routes/otpRoute");
-const likeRoute = require("./Routes/likeRoute");
+
+const userModel = require("./Models/userModel");
+const { chatRoomModel } = require("./Models/chatRoomModel");
+const { chat } = require("./Models/chatModel");
 
 ConnectToDB(process.env.URI);
 
@@ -22,7 +30,7 @@ ConnectToDB(process.env.URI);
 app.use(express.json());
 
 // MiddleWare for Cross Origins
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 
 //Middle to serve files on frontend
 app.use("/uploads", express.static("uploads"));
@@ -35,30 +43,55 @@ app.use("/api/", otpRoute);
 //Like Routes
 app.use("/api/like", likeRoute);
 
+//Chat Routes
+app.use("/api/chat", chatRoute);
+
 //Socket Implementation
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("New user Connected:", socket.id);
 
-  // Join a room using sender and receiver IDs
-  socket.on("join-room", ({ senderId, receiverId }) => {
-    const roomId = [senderId, receiverId].sort().join("_");
-    socket.join(roomId);
-    console.log(`User ${senderId} joined room ${roomId}`);
-  });
+  let userData;
+  const tokenData = socket?.handshake?.auth?.token;
+
+  if (tokenData) {
+    const token = tokenData?.split(" ")[1];
+    const tokenVerified = jwt.verify(token, process.env.SECRET_KEY);
+    if (tokenVerified) {
+      userData = await userModel.findById(tokenVerified?._id);
+      userData.socketId = socket.id;
+      await userData.save();
+    }
+    console.log("Token is verified at socket");
+  }
 
   // Send private message to the room
-  // socket.on("private-message", ({ senderId, receiverId, message }) => {
-  //   const roomId = [senderId, receiverId].sort().join("_");
-  //   io.to(roomId).emit("receive-message", { message, senderId });
-  //   console.log(`Message from ${senderId} to ${receiverId}: ${message}`);
-  // });
+  socket.on("private-message", async (data) => {
+    console.log("data is :", data);
 
-  socket.on("disconnect", () => {
+    const { senderId, roomId, message } = data;
+    const roomDetails = await chatRoomModel.findById(roomId);
+    const newChat = await chat.create(data);
+    const receiverId =
+      roomDetails?.senderId.toString() === senderId.toString()
+        ? roomDetails?.receiverId
+        : roomDetails?.senderId;
+    const receiverData = await userModel.findById(receiverId);
+    const senderData = await userModel.findById(senderId);
+    io.to(receiverData?.socketId).emit("receive_message", newChat);
+    io.to(senderData?.socketId).emit("receive_message", newChat);
+    console.log(`Message from ${senderId} to ${receiverId}: ${message}`);
+  });
+
+  socket.on("disconnect", async () => {
+    if (userData) {
+      userData.socketId = null;
+      await userData.save();
+    }
     console.log("User disconnected:", socket.id);
   });
 });
 
 //App is Running on this PORT
-server.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, async () => {
   console.log(`HeartBeat is Running on ${process.env.PORT}`);
 });
